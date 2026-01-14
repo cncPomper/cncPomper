2f473657d43e84b7dd0277d44573b8d1510fae03
Piotr Kitłowski <piotr.kitlowski@gmail.com>
2026-01-14 20:19:19 +0000

    init permutate

---
+++ learning/backtracking/backtracking.py
@@ -0,0 +1,14 @@
+# Permuate a list using backtracking
+def permute(nums):
+    def backtrack(start):
+        if start == len(nums):
+            result.append(nums[:])
+            return
+        for i in range(start, len(nums)):
+            nums[start], nums[i] = nums[i], nums[start]  # Swap
+            backtrack(start + 1)
+            nums[start], nums[i] = nums[i], nums[start]  # Backtrack
+
+    result = []
+    backtrack(0)
+    return result
