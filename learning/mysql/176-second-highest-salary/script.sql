41b53c20efa67da896ef01458e3ab2fe36c47536
Piotr Kitłowski <piotr.kitlowski@gmail.com>
2026-02-13 17:49:43 +0000

    176 mysql leetcode

---
+++ learning/mysql/176-second-highest-salary/script.sql
@@ -0,0 +1,8 @@
+Create table If Not Exists Employee (id int, salary int)
+Truncate table Employee
+insert into Employee (id, salary) values ('1', '100')
+insert into Employee (id, salary) values ('2', '200')
+insert into Employee (id, salary) values ('3', '300')
+
+SELECT (SELECT DISTINCT salary 
+FROM Employee ORDER BY salary DESC limit 1, 1) as SecondHighestSalary;
